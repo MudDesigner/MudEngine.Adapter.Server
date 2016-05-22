9359649784641d924d747bec4a5c62d63b5fedbe
Johnathon Sullinger <johnathon@sullinger.us>
2016-05-21 22:28:15 -0700

    Fixed libhostpolicy.dylib runtime error.

--- src/MudDesigner.Server/Program.cs
+++ src/MudDesigner.Server/Program.cs
@@ -5,42 +5,42 @@
 //-----------------------------------------------------------------------
 namespace MudDesigner.MudServer.Windows
 {
-    // using MudDesigner.MudEngine.Actors;
-    // using MudDesigner.MudEngine.MessageBrokering;
+    using MudDesigner.MudEngine.Actors;
+    using MudDesigner.MudEngine.MessageBrokering;
     using System;
     using System.Threading;
-    // using MudEngine;
+    using MudEngine;
     using System.Threading.Tasks;
     
     class MainClass
     {
         public static void Main(string[] args)
         {
-            // SetupMessageBrokering();
+            SetupMessageBrokering();
 
-            // var bootstrap = new Bootstrap();
-            // Task bootstrapTask = bootstrap.Initialize();
+            var bootstrap = new Bootstrap();
+            Task bootstrapTask = bootstrap.Initialize();
 
-            // Wait for the engine to shut down.
-            // bootstrapTask.Wait();
+            //Wait for the engine to shut down.
+            bootstrapTask.Wait();
         }
 
         static void SetupMessageBrokering()
         {
-            // MessageBrokerFactory.Instance.Subscribe<InfoMessage>(
-            //     (msg, subscription) => Console.WriteLine(msg.Content));
+            MessageBrokerFactory.Instance.Subscribe<InfoMessage>(
+                (msg, subscription) => Console.WriteLine(msg.Content));
 
-            // MessageBrokerFactory.Instance.Subscribe<GameMessage>(
-            //     (msg, subscription) => Console.WriteLine(msg.Content));
+            MessageBrokerFactory.Instance.Subscribe<GameMessage>(
+                (msg, subscription) => Console.WriteLine(msg.Content));
 
-            // MessageBrokerFactory.Instance.Subscribe<PlayerCreatedMessage>(
-            //     (msg, sub) => Console.WriteLine("Player connected."));
+            MessageBrokerFactory.Instance.Subscribe<PlayerCreatedMessage>(
+                (msg, sub) => Console.WriteLine("Player connected."));
 
-            // MessageBrokerFactory.Instance.Subscribe<PlayerDeletionMessage>(
-            //     (msg, sub) =>
-            //     {
-            //         Console.WriteLine("Player disconnected.");
-            //     });
+            MessageBrokerFactory.Instance.Subscribe<PlayerDeletionMessage>(
+                (msg, sub) =>
+                {
+                    Console.WriteLine("Player disconnected.");
+                });
         }
     }
 }
--- src/MudDesigner.Server/TestPlayerFactory.cs
+++ src/MudDesigner.Server/TestPlayerFactory.cs
@@ -1,19 +1,19 @@
-﻿// //-----------------------------------------------------------------------
-// // <copyright file="TestPlayerFactory.cs" company="Sully">
-// //     Copyright (c) Johnathon Sullinger. All rights reserved.
-// // </copyright>
-// //-----------------------------------------------------------------------
-// namespace MudDesigner.MudEngine.Networking
-// {
-//     using System;
-//     using MudDesigner.MudEngine.Actors;
-//     using Commanding;
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestPlayerFactory.cs" company="Sully">
+//     Copyright (c) Johnathon Sullinger. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace MudDesigner.MudEngine.Networking
+{
+    using System;
+    using MudDesigner.MudEngine.Actors;
+    using Commanding;
 
-//     public class TestPlayerFactory : IPlayerFactory
-//     {
-//         public IPlayer CreatePlayer(IActorCommand initialCommand)
-//         {
-//             return new TestPlayer();
-//         }
-//     }
-// }
+    public class TestPlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(IActorCommand initialCommand)
+        {
+            return new TestPlayer();
+        }
+    }
+}
